f03eebda8da0831646febdfb5f763209820f2b00
Emanuel Sousa <franciscoemanuel23@outlook.com>
2025-05-05 22:40:18 -0300

    update na estutura do código

--- src/routes/Paths.jsx
+++ src/routes/Paths.jsx
@@ -2,13 +2,13 @@ import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import Home from "../pages/Home";
 
 const Paths = () => {
-    return ( 
-        <Router>
-            <Routes>
-                <Route path="/" element={<Home />} />
-            </Routes>
-        </Router>
-     );
-}
- 
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
+  );
+};
+
 export default Paths;
