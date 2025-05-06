cdb6104cf316b4fb0c6f19c68761e5ea37577b0d
Emanuel Sousa <franciscoemanuel23@outlook.com>
2025-05-05 21:18:42 -0300

    correção de bug no Paths

--- src/routes/Paths.jsx
+++ src/routes/Paths.jsx
@@ -1,4 +1,4 @@
-import { Route, Router, Routes } from "react-router-dom";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import Home from "../pages/Home";
 
 const Paths = () => {
